15f4473968bbcef3ff596e740750a5eb58a2ac16
Hendi Santika <hendisantika@gmail.com>
2021-06-11 10:13:55 +0700

    #2 - Add required libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,16 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("com.github.kstyrc:embedded-redis:0.6")
 }
 
 tasks.withType<KotlinCompile> {
