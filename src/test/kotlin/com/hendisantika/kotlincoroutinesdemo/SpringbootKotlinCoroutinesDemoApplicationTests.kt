fc10860819ed0cd115901839ed0e38f5f2a12ea8
Hendi Santika <hendisantika@gmail.com>
2021-06-11 10:13:13 +0700

    #1 - Initial Commit

---
+++ src/test/kotlin/com/hendisantika/kotlincoroutinesdemo/SpringbootKotlinCoroutinesDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.kotlincoroutinesdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinCoroutinesDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
