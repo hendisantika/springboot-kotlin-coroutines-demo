44bc71c8798b2bb4919a1933ed9def97c59c6497
Hendi Santika <hendisantika@gmail.com>
2021-06-11 15:35:01 +0700

    #4 - Add CounterEvent DTO

---
+++ src/main/kotlin/com/hendisantika/kotlincoroutinesdemo/DataTransferObjects.kt
@@ -0,0 +1,22 @@
+package com.hendisantika.kotlincoroutinesdemo
+
+import java.time.ZoneId
+import java.time.ZonedDateTime
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-coroutines-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/06/21
+ * Time: 15.33
+ */
+
+class CounterEvent(
+    val value: Long,
+    val action: CounterAction,
+    val at: ZonedDateTime = ZonedDateTime.now(ZoneId.of("UTC"))
+)
+
+enum class CounterAction { UP, DOWN }
--- src/main/kotlin/com/hendisantika/kotlincoroutinesdemo/SpringbootKotlinCoroutinesDemoApplication.kt
+++ src/main/kotlin/com/hendisantika/kotlincoroutinesdemo/SpringbootKotlinCoroutinesDemoApplication.kt
@@ -1,10 +1,33 @@
 package com.hendisantika.kotlincoroutinesdemo
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.core.ReactiveRedisTemplate
 
 @SpringBootApplication
-class SpringbootKotlinCoroutinesDemoApplication
+class SpringbootKotlinCoroutinesDemoApplication {
+    @Bean
+    fun reactiveRedisTemplate(
+        connectionFactory: ReactiveRedisConnectionFactory,
+        objectMapper: ObjectMapper
+    ): ReactiveRedisTemplate<String, CounterEvent> {
+
+        val valueSerializer = Jackson2JsonRedisSerializer(CounterEvent::class.java).apply {
+            setObjectMapper(objectMapper)
+        }
+
+        return ReactiveRedisTemplate(
+            connectionFactory,
+            newSerializationContext<String, CounterEvent>(StringRedisSerializer())
+                .value(valueSerializer)
+                .build()
+        )
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringbootKotlinCoroutinesDemoApplication>(*args)
