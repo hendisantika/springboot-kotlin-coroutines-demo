c760e868d2bc377437d84e8810b5f15c850ec5d1
Hendi Santika <hendisantika@gmail.com>
2021-06-11 15:40:04 +0700

    #10 - Add up in CounterRepository

--- src/main/kotlin/com/hendisantika/kotlincoroutinesdemo/CounterRepository.kt
+++ src/main/kotlin/com/hendisantika/kotlincoroutinesdemo/CounterRepository.kt
@@ -2,6 +2,7 @@ package com.hendisantika.kotlincoroutinesdemo
 
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.data.redis.core.incrementAndAwait
+import org.springframework.data.redis.core.sendAndAwait
 import org.springframework.stereotype.Repository
 
 /**
@@ -16,6 +17,11 @@ import org.springframework.stereotype.Repository
 @Repository
 class CounterRepository(private val redisTemplate: ReactiveRedisTemplate<String, CounterEvent>) {
     suspend fun get(): CounterState = CounterState(redisTemplate.opsForValue().incrementAndAwait(COUNTER_KEY, 0L))
+
+    suspend fun up(): CounterState =
+        CounterState(redisTemplate.opsForValue().incrementAndAwait(COUNTER_KEY)).also {
+            redisTemplate.sendAndAwait(COUNTER_CHANNEL, it.toEvent(CounterAction.UP))
+        }
 
     companion object {
         private const val COUNTER_CHANNEL = "COUNTER_CHANNEL"
